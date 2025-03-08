4c9b9eaf4b148d28a792fca605023582cf501e57
GOPAL-YADAV-D <gopalyadav.d.888@gmail.com>
2025-03-08 13:00:53 +0530

    Add vercel.json and update auth

--- server.js
+++ server.js
@@ -2,16 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
-
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
@@ -32,7 +29,14 @@ app.get("/", (req, res) => {
   res.send("IEEE VIT Enrollment 2025");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
-  connectDB();
-});
+
+connectDB();
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
